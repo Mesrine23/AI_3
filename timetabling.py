2f67feac979b46a51ce61e5f36d9cc761cc8ca03
Mesrine23 <palas.pan.23@gmail.com>
2021-12-20 13:57:33 +0200

    update

---
+++ timetabling.py
@@ -0,0 +1,27 @@
+import csp
+import search
+import utils
+import csv
+
+semester = []
+variables = []
+professors = []
+difficulty = []
+laboratory = []
+
+with open('Στοιχεία Μαθημάτων.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if (line_count != 0):
+            semester.append(row[0])
+            variables.append(row[1])
+            professors.append(row[2])
+            difficulty.append(row[3])
+            laboratory.append(row[4])
+        line_count += 1
+    
+for count in range(0,line_count-1):
+    print(semester[count],variables[count],professors[count],difficulty[count],laboratory[count])
+    
+
