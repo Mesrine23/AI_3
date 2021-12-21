039bb33205f172625dde5a21ce090d1dbd81444e
Mesrine23 <palas.pan.23@gmail.com>
2021-12-21 17:19:06 +0200

    created neighbors

--- timetabling.py
+++ timetabling.py
@@ -6,7 +6,38 @@ import csv
 class Problem(csp.CSP):
     
     def __init__(self, semester, courses, professors, difficulty, lab):
-        csp.__init__(self, courses)
+        self.variables = []
+        self.domains = {}
+        self.neighbors = {}
+
+        #init variables with courses
+        self.variables = courses
+
+        #init domain
+        slots = []
+        lab_slots = []
+        for i in range(1,22):
+            new_slot = (i,'9-12')
+            slots.append(new_slot)
+            lab_slots.append(new_slot)
+            new_slot = (i,'12-3')
+            slots.append(new_slot)
+            lab_slots.append(new_slot)
+            new_slot = (i,'3-6')
+            slots.append(new_slot)
+        dom = {}
+        for i in range(len(courses)):
+            if lab[i] == "TRUE":
+                dom[courses[i]] = lab_slots
+            else:
+                dom[courses[i]] = slots
+        self.domains = dom
+
+        #init neighbors
+        #for i in range(len(courses)):
+            
+
+        csp.CSP.__init__(self , self.variables , self.domains)
 
 
 
@@ -34,28 +65,22 @@ if __name__=='__main__':
                 lab.append(row[4])
             line_count += 1
 
-    slots = []
-    lab_slots = []
-    for i in range(1,22):
-        new_slot = (i,'9-12')
-        slots.append(new_slot)
-        lab_slots.append(new_slot)
-        new_slot = (i,'12-3')
-        slots.append(new_slot)
-        lab_slots.append(new_slot)
-        new_slot = (i,'3-6')
-        slots.append(new_slot)
+    neighb = {}
+    for nb in range(len(courses)):
+        neighbs = []
+        for crs in range(len(courses)):
+            if(courses[nb]!=courses[crs]):
+                if(semester[nb]==semester[crs]):
+                    neighbs.append(courses[crs])
+                elif(professors[nb]==professors[crs]):
+                    neighbs.append(courses[crs])
+                elif(difficulty[nb]=="TRUE" and difficulty[crs]=="TRUE"):
+                    neighbs.append(courses[crs])
+        neighb[courses[nb]] = neighbs
 
-    dict = {}
-    for i in range(len(courses)):
-        if lab[i] == "TRUE":
-            dict[courses[i]] = lab_slots
-        else:
-            dict[courses[i]] = slots
-
-    print(dict)
-    
-
+    for course in courses:
+        print(neighb[course])
+        print()
 
     #print("dict ->",dict)
 
