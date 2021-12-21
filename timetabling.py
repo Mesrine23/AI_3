eefcc89043e6959a45c5c5e54f92e9016c107eb4
Mesrine23 <palas.pan.23@gmail.com>
2021-12-21 14:30:04 +0200

    made dict{}

--- timetabling.py
+++ timetabling.py
@@ -3,25 +3,63 @@ import search
 import utils
 import csv
 
-semester = []
-variables = []
-professors = []
-difficulty = []
-laboratory = []
+class Problem(csp.CSP):
+    
+    def __init__(self, semester, courses, professors, difficulty, lab):
+        csp.__init__(self, courses)
 
-with open('Στοιχεία Μαθημάτων.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if (line_count != 0):
-            semester.append(row[0])
-            variables.append(row[1])
-            professors.append(row[2])
-            difficulty.append(row[3])
-            laboratory.append(row[4])
-        line_count += 1
 
-for count in range(0,line_count-1):
-    print(semester[count],variables[count],professors[count],difficulty[count],laboratory[count])
+
+
+
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+
+if __name__=='__main__':
+
+    semester = []
+    courses = []
+    professors = []
+    difficulty = []
+    lab = []
+
+    with open('Στοιχεία Μαθημάτων.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if (line_count != 0):
+                semester.append(row[0])
+                courses.append(row[1])
+                professors.append(row[2])
+                difficulty.append(row[3])
+                lab.append(row[4])
+            line_count += 1
+
+    slots = []
+    lab_slots = []
+    for i in range(1,22):
+        new_slot = (i,'9-12')
+        slots.append(new_slot)
+        lab_slots.append(new_slot)
+        new_slot = (i,'12-3')
+        slots.append(new_slot)
+        lab_slots.append(new_slot)
+        new_slot = (i,'3-6')
+        slots.append(new_slot)
+
+    dict = {}
+    for i in range(len(courses)):
+        if lab[i] == "TRUE":
+            dict[courses[i]] = lab_slots
+        else:
+            dict[courses[i]] = slots
+
+    print(dict)
+    
+
+
+    #print("dict ->",dict)
+
+    #problem = Problem(semester,courses,professors,difficulty,lab)
+    
     
 
